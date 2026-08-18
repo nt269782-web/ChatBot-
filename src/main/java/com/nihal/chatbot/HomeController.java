f0b8c33b75181ce5d7f4c7d35caffb2ca7cf3639
Nihal Tiwari <nt269782@gmail.com>
2026-08-18 14:36:41 +0530

    Add files via upload

---
+++ src/main/java/com/nihal/chatbot/HomeController.java
@@ -0,0 +1,13 @@
+package com.nihal.chatbot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Chatbot Backend is running!";
+    }
+}
